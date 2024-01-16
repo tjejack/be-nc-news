01bb70b69c700306f5dad47d11fcb0134958938c
tjejack <tjeidanjack@gmail.com>
2024-01-16 11:33:00 +0000

    functionality for GET /api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -6,27 +6,21 @@ const {
   getArticles,
   getArticleByArticleId,
 } = require("./controllers/articles-controllers.js");
+const { getArticleComments } = require("./controllers/comments-controllers.js");
 
 app.get("/api", getEndpoints);
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleByArticleId);
+app.get("/api/articles/:article_id/comments", getArticleComments)
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Endpoint Not Found" });
 });
 
 app.use((err, req, res, next) => {
-  if (err.status === 400) {
-    res.status(400).send({ msg: "Bad Request" });
-  } else {
-    next(err);
-  }
-});
-
-app.use((err, req, res, next) => {
-  if (err.status === 404) {
-    res.status(404).send({ msg: "Not Found" });
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
   } else {
     next(err);
   }
