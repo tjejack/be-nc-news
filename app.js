201bedde17619866450c69863a7e18d8213e6318
tjejack <tjeidanjack@gmail.com>
2024-01-17 10:27:48 +0000

    functionality for task 10 GET /api/users

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ const {
   patchArticle
 } = require("./controllers/articles-controllers.js");
 const { getArticleComments, postComment } = require("./controllers/comments-controllers.js");
+const {getUsers} = require("./controllers/users-controllers.js")
 
 app.use(express.json())
 
 app.get("/api", getEndpoints);
 app.get("/api/topics", getTopics);
+app.get("/api/users", getUsers);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleByArticleId);
 app.get("/api/articles/:article_id/comments", getArticleComments)
+
+
 app.post("/api/articles/:article_id/comments", postComment)
+
 app.patch("/api/articles/:article_id", patchArticle)
 
 app.all("*", (req, res) => {
--- models/users-models.js
+++ models/users-models.js
@@ -1,9 +1,20 @@
 const db = require("../db/connection.js");
 
 module.exports.checkUserExists = (username) => {
-  return db.query(`SELECT * FROM users WHERE username = $1`, [username]).then(({ rows }) => {
-    if (rows.length === 0) {
-      return Promise.reject({ status: 400, msg: "Bad Request - No Such User" });
-    }
+  return db
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 400,
+          msg: "Bad Request - No Such User",
+        });
+      }
+    });
+};
+
+module.exports.fetchUsers = () => {
+  return db.query(`SELECT * FROM users`).then(({ rows }) => {
+    return rows;
   });
 };
