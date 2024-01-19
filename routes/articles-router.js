f765eee6aa8c7bfc7387926ea7d0af7c04b35e19
tjejack <tjeidanjack@gmail.com>
2024-01-19 14:16:47 +0000

    create endpoint for POST /api/articles

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -2,6 +2,7 @@ const {
   getArticles,
   getArticleByArticleId,
   patchArticle,
+  postArticle,
 } = require("../controllers/articles-controllers.js");
 const {
   getArticleComments,
@@ -9,7 +10,7 @@ const {
 } = require("../controllers/comments-controllers.js");
 const articlesRouter = require("express").Router();
 
-articlesRouter.get("/", getArticles);
+articlesRouter.route("/").get(getArticles).post(postArticle);
 
 articlesRouter
   .route("/:article_id")
